33b3dc5c011f66b9bbf812825b5b9f73752914b8
Mounir Karoui <mounir.karoui@gmx.de>
2023-11-27 11:38:59 +0100

    Just added line for other scenario

--- Source/Runners/DimensionalSplitting-Runner.cpp
+++ Source/Runners/DimensionalSplitting-Runner.cpp
@@ -57,6 +57,7 @@ int main(int argc, char** argv) {
   if (checkpointFile.empty()) {
     auto tsunamiScenario = new Scenarios::TsunamiScenario();
     tsunamiScenario->readScenario("chile_gebco_usgs_2000m_bath.nc", "chile_gebco_usgs_2000m_displ.nc");
+    //tsunamiScenario->readScenario("tohoku_gebco_ucsb3_2000m_hawaii_bath.nc", "tohoku_gebco_ucsb3_2000m_hawaii_displ.nc");
     scenario = tsunamiScenario;
   } else {
     scenario = new Scenarios::CheckpointScenario(checkpointFile);
